74ca96046add93eebc0ae47588e8fcfe9194ec4e
Harro114 <46030262+Harro114@users.noreply.github.com>
2019-04-22 20:54:21 +0300

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import datetime # работа с датой и временем
 import time
 
 while True:
-    vk = vk_api.VkApi(token="cce4bc728b7729d3a230cd342d8bfef7c51c7faa1a324d6c63ec53bb46181b465367942dba605b7cab9ca")
+    vk = vk_api.VkApi(token="text")
     delta = datetime.timedelta(hours=3, minutes=0) # разница от UTC. Можете вписать любое значение вместо 3
     t = (datetime.datetime.now(datetime.timezone.utc) + delta) # Присваиваем дату и время переменной «t»
     nowtime = t.strftime("%H:%M") # текущее время
